b4f855c1bb8e30f9970da98611687e411b78eb58
hacheDJ <dejotahachece@gmail.com>
2023-10-24 19:10:02 -0500

    ssl config

--- db/sequelize.mysql.js
+++ db/sequelize.mysql.js
@@ -7,7 +7,12 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+          }
     }
 )
 
