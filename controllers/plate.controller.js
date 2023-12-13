fe67b24d8636da3246ff2e0ccdd1b2bd11ae1fef
hacheDJ <dejotahachece@gmail.com>
2023-12-13 11:53:36 -0500

    multer/firebase

--- controllers/plate.controller.js
+++ controllers/plate.controller.js
@@ -39,15 +39,17 @@ const addCtrl =  async (req = request, res = response) => {
 
             /* const downloadUrl = uploadedFile.metadata.mediaLink
             return res.json({err: false, msg: `Subido al url: ${downloadUrl}`}) */
-           }   
-        )
 
-        const plate = {namePlate, descriptionPlate, price, photo: modifiedName, state: "disponible"}
+            const plate = {namePlate, descriptionPlate, price, photo: modifiedName, state: "disponible"}
         const plateRegister = await Plate.create(plate)
 
         /* console.log('NEW PLATE', plateRegister) */
 
         return res.json({err: false, msg: `Se agrego el plato con id ${plateRegister.dataValues.id}`})
+           }   
+        )
+
+        
     } catch (err) {
         res.json({err: true, msg: err.message})
     }
