ce849d96d962f8cd905fe0e79a5df0e13b280ac8
hacheDJ <dejotahachece@gmail.com>
2023-12-12 23:44:59 -0500

    .

--- handlers/aws.js
+++ handlers/aws.js
@@ -1,6 +1,30 @@
 require('dotenv').config()
 
 const aws = require('aws-sdk')
+
+// Configura las credenciales
+const credentials = new aws.Credentials({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN // Opcional, solo si estás utilizando credenciales temporales
+  });
+  
+  // Configura el SDK de AWS con las credenciales
+  aws.config.credentials = credentials;
+  
+  // Crea una instancia de IAM (Identity and Access Management)
+  const iam = new aws.IAM();
+  
+  // Obtiene los detalles de la cuenta
+  iam.getAccountSummary({}, (err, data) => {
+    if (err) {
+      console.error('Error al obtener detalles de la cuenta:', err);
+    } else {
+      console.log('ID de cuenta:', data.SummaryMap.AccountId);
+      console.log('Alias de cuenta:', data.SummaryMap.AccountAlias);
+    }
+  })
+
 const s3 = new aws.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
