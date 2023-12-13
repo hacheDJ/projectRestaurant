4501cc823e33c83e14d2e13ace2ed05e4d98e158
hacheDJ <dejotahachece@gmail.com>
2023-12-13 00:00:57 -0500

    .

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const aws = require('aws-sdk')
 
 // Configura las credenciales
 const credentials = new aws.Credentials({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    sessionToken: process.env.AWS_SESSION_TOKEN // Opcional, solo si estás utilizando credenciales temporales
+    accessKeyId: 'ASIAY3DGHTVYAKOJZCEI',
+    secretAccessKey: 'yrAbuHie05yUkP63E2JKOzoK20PWlzxzVXhYKbfj',
+    sessionToken: 'IQoJb3JpZ2luX2VjEKv//////////wEaCXNhLWVhc3QtMSJHMEUCIQClhAPMi+4h0sIVa+2ClghHsNLMmFHtrZdT8Ioje74YKAIgPIQU27RMOX0SenJbdWiCchrkhTkbVYZ7sZCSC93yEpkqrQIIJBAAGgw2MDc5NTIyODA5NDQiDCkX3n/b/F1x3JQCWSqKAkH7iBj5s2Bqhz59siMakzedTi4ykYZC3xJNgq4q1YVR68nGVvPEE4bWAGjngmZYh0c7KTiRWX9oula2/OLRUS2f8esVErcAfWGq5XBm0zTCm+B+bUi2BEzU6wG6femJV4JQtt3gjanZio2jDDleOLh1e3tpcUOb4Ggb8uKVDvyvrGqcVtx6K36vIZ8DK2Usz13CIDVg5K391SBnsJRzmJishxwWv0y7UKKoCN74EcEy2GcNZZg722Q8rsKkI4iptgGKpo7C3H7T6ub8UKAqlegfv2XgXKc78P2+RYcs+dJwy1JTXhttaH/CoDEHgMhVYz9JSNvHbf3z5+ZfekCJev1oVBDZgCl5+yLcMMab36sGOp0Bkd8lWwOg5oExZzDTSdL+FyzSTHeCrR4DITf/DoISjpM0QqfDqDdT7EsbWm7ApsNhf49HwYY/oK9Eb62HbL9o7zpayXE1lWiafAdBheR2FHSXFrgyRb13E5OTRJkHJ/7IXtEZXPBvUCaybRBXh9BWL8gInWH18NWtLsJ3PsZaOZB1UVkiwE4UM1jnrOoGCXKwmIlESoeY1ReMpduXmg==' // Opcional, solo si estás utilizando credenciales temporales
   });
   
   // Configura el SDK de AWS con las credenciales
